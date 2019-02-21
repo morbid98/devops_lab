b9a61b6559b427f60676fc390f17f91a35e9bcf1
Student <student@localhost.localdomain>
2019-02-21 14:48:05 +0300

    second

--- task4.py
+++ task4.py
@@ -1,5 +1,5 @@
 n = int(input())
-pr="{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}"
+pr = "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}"
 w = len("{0:b}".format(n))
 for i in range(1, n + 1):
     print(pr.format(i, width=w))
