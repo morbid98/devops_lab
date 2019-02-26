2aa11357103acb5120457baa689c844fd89d3ee9
Student <student@localhost.localdomain>
2019-02-26 17:17:19 +0300

    first try

---
+++ scr.py
@@ -0,0 +1,50 @@
+import requests as req
+import getpass
+
+user = input("Your username ")
+password = getpass.getpass()
+rep_user = input("Someones git ")
+rep = input("Repository ")
+git = 'https://api.github.com/'
+pull_path = 'repos/' \
+            + rep_user \
+            + '/' \
+            + rep \
+            + '/pulls'
+surname = input("Enter something to search in title ")
+search_pat2 = req.get(git +
+                      'search/issues'
+                      '?q=+type:pr+repo:'
+                      + rep_user
+                      + '/'
+                      + rep
+                      + "+state:open"
+                        "+"
+                      + surname
+                      + "in:title"
+                        '&sort=created&order=asc',
+                      auth=(user, password)).json()
+found_total = search_pat2['total_count']
+for i in range(found_total):
+    number = search_pat2['items'][i]['number']
+    pull_info = req.get(git
+                        + pull_path
+                        + '/'
+                        + str(number),
+                        auth=(user, password)).json()
+    data = req.get(
+        git + pull_path + '/' + str(number) + '/files',
+        auth=(user, password)).json()
+    print("\nPull request %s info" % number)
+    print("======================")
+    print("Title: %s" % (str(pull_info['title'])))
+    print("Create time: %s" % (str(pull_info['created_at'])))
+    print("Update time: %s" % (str(pull_info['updated_at'])))
+    for j in data:
+        print("Changed %s file" % str(j['filename']))
+        print("%s lines added" % str(j['additions']))
+        print("%s lines deleted" % str(j['deletions']))
+    else:
+        print("=======================")
+else:
+    print("Done")
